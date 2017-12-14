27025223374fa4d5257e40d5ed0c21de1dcdfa7f
sarahschaller <sarcsch@aol.com>
2017-12-14 13:07:30 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def user_attributes=(user_attributes)
+  	user_attributes.values.each do |user_attribute|
+
+  		if user_attribute.present?
+  			user = User.find_or_create_by(:username => user_attribute)
+  			user.comments << self
+  		end 
+  	end
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,23 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories 
+
+  def categories_attributes=(categories_hashes)
+    # categories_hashes = {"0"=>{"name"=>"new category 1"}, "1"=>{"name"=>"new category 2"}}
+    categories_hashes.each do |index, category_attributes|
+      if category_attributes[:name].present?
+
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        
+        if !self.categories.include?(category)
+          # self.categories << category
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+
+  end
 
 
 end
